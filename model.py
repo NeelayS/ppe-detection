1909c66a8f47fa67ef195441f71b341b54d3e0fb
NeelayS <shahnh19@gmail.com>
2022-05-23 12:15:45 +0530

    Replaced YOLO detection model with Pose+Detection model

--- model.py
+++ model.py
@@ -1,12 +1,79 @@
 import cv2
 import os
-from pytorchyolo import detect, models as yolo_models
 import torch
 from torch import nn
 import torch.nn.functional as F
 from torchvision import io, models
 
 from backbone import Backbone
+
+
+class PoseDetectionModel:
+    def __init__(self, device=torch.device("cpu"), conf_thresh=0.5):
+
+        self.model = models.detection.keypointrcnn_resnet50_fpn(pretrained=True).eval()
+        self.model.to(device)
+
+        self.conf_thresh = conf_thresh
+
+    def _filter_detections(self, results):
+
+        filtered_detections = []
+
+        for i in range(len(results)):
+
+            if (
+                results["scores"][i] < self.conf_thresh
+                or int(results["labels"][i]) != 1
+            ):
+                continue
+
+            keypoints = results["keypoints"][i]
+
+            if (
+                int(keypoints[0][2])
+                == int(keypoints[-2][2])
+                == int(keypoints[-1][2])
+                == 1
+            ):
+
+                det_box = [max(0, int(coord)) for coord in results["boxes"][i]]
+                filtered_detections.append(det_box)
+
+        return filtered_detections
+
+    def __call__(self, img=None, img_path=None, return_detections=False):
+
+        assert (
+            img is not None or img_path is not None
+        ), "Either img or img_path must be provided"
+
+        if img is None:
+            img = io.imread(img_path)
+
+        if not isinstance(img, torch.Tensor):
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = torch.from_numpy(img).permute(2, 0, 1).float()
+            img = img.unsqueeze(0)
+
+        n_img = img / 255.0
+        n_img = img.to(self.device)
+
+        out = self.model(n_img)[0]
+        filtered_detections = self._filter_detections(out)
+
+        cropped_detections = []
+        for detection in filtered_detections:
+            cropped_detections.append(
+                img[:, detection[1] : detection[3], detection[0] : detection[2]]
+                .unsqueeze(0)
+                .float()
+            )
+
+        if return_detections:
+            return cropped_detections, filtered_detections
+
+        return cropped_detections
 
 
 class ClassificationHead(nn.Module):
@@ -35,51 +102,51 @@ class ClassificationHead(nn.Module):
         return self.head(x)
 
 
-class YoloV3DetectionModel:
-    def __init__(self, config_path, weights_path, threshold=0.5):
+# class YoloV3DetectionModel:
+#     def __init__(self, config_path, weights_path, threshold=0.5):
 
-        self.threshold = threshold
+#         self.threshold = threshold
 
-        self.model = yolo_models.load_model(config_path, weights_path)
+#         self.model = yolo_models.load_model(config_path, weights_path)
 
-    def _filter_detections(self, detections):
+#     def _filter_detections(self, detections):
 
-        filtered_detections = []
+#         filtered_detections = []
 
-        for detection in detections:
+#         for detection in detections:
 
-            if (
-                detection[-2] > self.threshold and int(detection[-1]) == 0
-            ):  # 0 = person class
-                filtered_detection = list(map(lambda x: max(int(x), 0), detection[:-2]))
-                filtered_detections.append(filtered_detection)
+#             if (
+#                 detection[-2] > self.threshold and int(detection[-1]) == 0
+#             ):  # 0 = person class
+#                 filtered_detection = list(map(lambda x: max(int(x), 0), detection[:-2]))
+#                 filtered_detections.append(filtered_detection)
 
-        return filtered_detections
+#         return filtered_detections
 
-    def __call__(self, img_path, return_detections=False):
+#     def __call__(self, img_path, return_detections=False):
 
-        img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         img = cv2.imread(img_path)
+#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        detections = detect.detect_image(self.model, img)
-        detections = self._filter_detections(detections)
+#         detections = detect.detect_image(self.model, img)
+#         detections = self._filter_detections(detections)
 
-        print(f"Detected {len(detections)} persons")
+#         print(f"Detected {len(detections)} persons")
 
-        img = io.read_image(img_path)
+#         img = io.read_image(img_path)
 
-        cropped_detections = []
-        for detection in detections:
-            cropped_detections.append(
-                img[:, detection[1] : detection[3], detection[0] : detection[2]]
-                .unsqueeze(0)
-                .float()
-            )
+#         cropped_detections = []
+#         for detection in detections:
+#             cropped_detections.append(
+#                 img[:, detection[1] : detection[3], detection[0] : detection[2]]
+#                 .unsqueeze(0)
+#                 .float()
+#             )
 
-        if return_detections:
-            return cropped_detections, detections
+#         if return_detections:
+#             return cropped_detections, detections
 
-        return cropped_detections
+#         return cropped_detections
 
 
 class ClassificationModel(nn.Module):
@@ -129,27 +196,24 @@ class CompleteModel(nn.Module):
     def __init__(
         self,
         detection_threshold,
-        detection_config,
-        detection_weights,
         detection_reshape_size,
         classification_features_dim,
         classification_layers_config,
         classification_n_heads,
         classification_model_weights=None,
+        device=torch.device("cpu"),
     ):
         super().__init__()
 
-        self.detection_model = YoloV3DetectionModel(
-            config_path=detection_config,
-            weights_path=detection_weights,
-            threshold=detection_threshold,
+        self.detection_model = PoseDetectionModel(
+            conf_threshold=detection_threshold, device=device
         )
         self.classification_model = ClassificationModel(
             reshape_size=detection_reshape_size,
             features_dim=classification_features_dim,
             layers_config=classification_layers_config,
             n_heads=classification_n_heads,
-        )
+        ).to(device)
 
         if classification_model_weights is not None:
             self.classification_model.load_state_dict(
@@ -169,7 +233,7 @@ class Predictor:
         self, det_model_params, class_model_params, classification_model_weights=None
     ):
 
-        self.det_model = YoloV3DetectionModel(**det_model_params)
+        self.det_model = PoseDetectionModel(**det_model_params)
         self.class_model = ClassificationModel(**class_model_params)
 
         if classification_model_weights is not None:
